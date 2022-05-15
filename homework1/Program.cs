1309a053b7928f86cad4521a51227368ea34514b
1 <bondarenko-1994alenka@mail.ru>
2022-05-15 18:47:45 +0300

    Homework1

---
+++ homework1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Напишите код, который на вход принимает два числа и выдает. какое число большее, а какое меньшее
+*/
+
+Console.WriteLine("Введите для сравнения два числа:");
+
+int number1=Convert.ToInt32(Console.ReadLine());
+int number2=Convert.ToInt32(Console.ReadLine());
+
+int max=number1;
+if (max<number2) max=number2;
+Console.WriteLine("Максимальное число:"+ max);
