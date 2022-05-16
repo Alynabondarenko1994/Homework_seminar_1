70e3a3003b776caeb76b87959f871b1c8d537bcf
1 <bondarenko-1994alenka@mail.ru>
2022-05-16 11:49:30 +0300

    homework4

---
+++ homework4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу , которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+
+Console.WriteLine("Введите число:");
+
+int number=Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Четные числа:"); 
+for (int i=1; i<number;i++)
+{
+    
+if (i%2==0) 
+{
+    Console.Write(" "+i);
+}
+}
