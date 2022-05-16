40e988b1bd7041cb6635e6f58d9a3f2952e692ca
1 <bondarenko-1994alenka@mail.ru>
2022-05-16 11:36:46 +0300

    homework3

---
+++ homework2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу , которая на вход принимает три числа, и выдает макисмальное из этих чисел
+*/
+
+
+
+Console.WriteLine("Введите для сравнения три числа:");
+
+double number1=Convert.ToDouble(Console.ReadLine());
+double number2=Convert.ToDouble(Console.ReadLine());
+double number3=Convert.ToDouble(Console.ReadLine());
+
+double max=number1;
+if (max<number2) max=number2;
+if (max<number3) max=number3;
+
+Console.WriteLine("Наибольшее число:"+ max);
